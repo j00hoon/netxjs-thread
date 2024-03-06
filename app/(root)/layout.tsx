dec3badb869273f2425fc055b960f49463a12f0c
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-05 22:15:32 -0500

    Topbar Bottombar LeftSidebar RightSidebar are added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,20 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
 import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
 
 export const metadata = {
     title : "Threads app",
     description : "My thread app"
 }
 
+
 const inter = Inter({ subsets: ["latin"]})
+
 
 export default function RootLayout({ 
     children 
@@ -18,10 +25,95 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={`inter.className bg-dark-1`}>
-                    {children}
+                <body className={`inter.className`}>
+                    <Topbar />
+
+                    <main>
+                        <LeftSidebar />
+                        
+                        <section className="main-container">
+                            <div className="w-full max-w-4xl">
+                                {children}
+                            </div>
+                        </section>
+
+                        <RightSidebar />
+                    </main>                    
+
+                    <Bottombar />
                 </body>
             </html>
         </ClerkProvider>
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import { ClerkProvider } from "@clerk/nextjs";
+
+// import "../globals.css";
+// import LeftSidebar from "@/components/shared/LeftSidebar";
+// import Bottombar from "@/components/shared/Bottombar";
+// import RightSidebar from "@/components/shared/RightSidebar";
+// import Topbar from "@/components/shared/Topbar";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Threads",
+//   description: "A Next.js 13 Meta Threads application",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <ClerkProvider
+      
+//     >
+//       <html lang='en'>
+//         <body className={inter.className}>
+//           <Topbar />
+
+//           <main className='flex flex-row'>
+//             <LeftSidebar />
+//             <section className='main-container'>
+//               <div className='w-full max-w-4xl'>{children}</div>
+//             </section>
+//             {/* @ts-ignore */}
+//             <RightSidebar />
+//           </main>
+
+//           <Bottombar />
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,11 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    // <div className="h-screen">
+    //   <UserButton afterSignOutUrl="/" />
+    // </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
