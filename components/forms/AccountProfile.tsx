0f78d3b6f3e07f37635d30d488d88db731869aaa
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-07 21:55:38 -0500

    Update changing profile image'

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,7 +16,7 @@ import { UserValidation } from "@/lib/validations/user";
 
 import { z } from "zod"
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 
 
@@ -36,6 +36,9 @@ import { ChangeEvent } from "react";
    
 
     const AccountProfile = ({ user, btnTitle } : Props) => {
+
+        const [files, setFiles] = useState<File[]>([]);
+
         const form = useForm({
         resolver : zodResolver(UserValidation),
             defaultValues : {
@@ -47,8 +50,26 @@ import { ChangeEvent } from "react";
         });
 
 
-    const handleImage = (e : ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e : ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "";
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
